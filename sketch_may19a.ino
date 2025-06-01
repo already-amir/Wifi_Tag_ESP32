5389f9394e5c208184d63f3722a5ae867805b813
already-amir <already.amirm@gmail.com>
2025-06-01 18:47:20 +0330

    Update sketch_may19a.ino

--- sketch_may19a.ino
+++ sketch_may19a.ino
@@ -1,9 +1,9 @@
 
 #include <WiFi.h>
-#include<WebServer.h>
+#include <WebServer.h>
 #include <Preferences.h>
 
-#define  led_pin 2
+#define led_pin 2
 
 
 #define ws WebServer
@@ -11,13 +11,13 @@
 
 
 Preferences pr;
-//ws server(80);
+ws server(80);
 
 String e_ssid= "esp32";
 String e_pass= "123456789";
 String admnpass= "1234";
 
-string w_ssid=
+
 
 String ip="";
 long long logintime=0;
@@ -42,35 +42,6 @@ void setup() {
 
   pinMode(led_pin, OUTPUT);
   digitalWrite(led_pin,0);
-
-  pr.begin("esp",false);
-
-  e_pass=pr.getString("e_pass",e_pass);
-
-
-  unsigned long start=millis();
-
-
-
-
-
-
-
-
-
-
-  Serial.println(e_ssid);
-  pr.begin("esp_config",false);
-
-  pr.putString("ssid","12345");
-
-  e_ssid=pr.getString("ssid");
-
-  Serial.println(e_ssid);
-
-
-  pr.end();
-
 
 
   
